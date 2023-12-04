a62b5b4dfec55a1fccbac0fc846e5f32dee0a7f5
sba23313 <sba23313@student.cct.ie>
2023-12-04 22:52:36 +0000

    Initial commit

---
+++ src/homeworktomi/HomeworkTomi.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package homeworktomi;
+
+/**
+ *
+ * @author palic
+ */
+public class HomeworkTomi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String intro = """
+                       Al's all wet. Oh, why is Al all wet? Oh,
+                       Al's all wet 'cause he's standing in the rain.""";
+        
+        for (int verse = 1; verse <= 3; verse++) {
+            System.out.println(intro);
+            System.out.println("Why is Al out in the rain?");
+            
+            System.out.println((String) switch (verse) {
+                case 1 -> "That's because he has no brain.";
+                case 2 -> "That's because he is a pain.";
+                case 3 -> "Cause this is the last refrain.";
+                default -> "";
+            });
+            
+
+            
+            System.out.println("""
+                               In the rain, in the rain.
+                               Oh, oh, oh, oh""");
+            System.out.println();
+        }
+        
+        System.out.println(intro);
+    }
+    
+}
